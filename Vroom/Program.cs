58897dd60b0056a8c18322ffb087ed61221fdd9f
Nimisha Jacob <nimis1234@gmail.com>
2022-12-07 09:07:10 -0600

    commit to develop

--- Vroom/Program.cs
+++ Vroom/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Vroom
 {
@@ -16,6 +10,7 @@ namespace Vroom
             CreateHostBuilder(args).Build().Run();
         }
 
+        // create host builder
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
